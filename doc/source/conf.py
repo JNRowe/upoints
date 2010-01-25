3b60ec5cee901de880631aedcf37c3b7c26177fb
James Rowe <jnrowe@gmail.com>
2010-01-25 07:41:56 +0000

    Removed redundancy in extensions definition.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,9 +23,10 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.doctest',
-    'sphinx.ext.ifconfig', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
-    'sphinxcontrib.aafig']
+extensions = ["sphinx.ext.%s" % ext for ext in
+              ["autodoc", "coverage", "doctest", "ifconfig", "intersphinx",
+               "todo"]] + \
+             ["sphinxcontrib.%s" % ext for ext in ["aafig", ]]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
