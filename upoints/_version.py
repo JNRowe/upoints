493d0b9bed5bec1a9e285c099f2bd2affc4cf0cf
James Rowe <jnrowe@gmail.com>
2014-01-31 15:10:10 +0000

    Release 0.12.1

--- upoints/_version.py
+++ upoints/_version.py
@@ -1,10 +1,10 @@
-# This is upoints version 0.12.0 (2014-01-27)
+# This is upoints version 0.12.1 (2014-01-31)
 # pylint: disable=C0103, C0111, C0121, W0622
 
-dotted = '0.12.0'
-libtool = '12:20'
-hex = 0x000c00
-date = '2014-01-27'
-tuple = (0, 12, 0)
-dict = {'major': 0, 'micro': 0, 'minor': 12}
-web = 'upoints/0.12.0'
+dotted = '0.12.1'
+libtool = '12:21'
+hex = 0x000c01
+date = '2014-01-31'
+tuple = (0, 12, 1)
+dict = {'major': 0, 'micro': 1, 'minor': 12}
+web = 'upoints/0.12.1'
