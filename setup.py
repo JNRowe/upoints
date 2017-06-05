6bb9b7277d8a52bea55782dc91eeaea8f13f2bca
James Rowe <jnrowe@gmail.com>
2017-06-05 15:23:14 +0100

    Mark as Python 3.{5,6} compatible

--- setup.py
+++ setup.py
@@ -86,6 +86,8 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Database',
         'Topic :: Database :: Database Engines/Servers',
         'Topic :: Scientific/Engineering',
